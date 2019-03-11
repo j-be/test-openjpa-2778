4a35716188d2fe19a100a7fdec729b074e37336a
Juri Berlanda <juri.berlanda@tuwien.ac.at>
2019-03-11 17:38:39 +0100

    [Dependencies] Reduce dependencies.
    Signed-off-by: Juri Berlanda <juri.berlanda@tuwien.ac.at>

--- src/main/java/org/duckdns/owly/bugs/openjpa_2778/service/SomeRepository.java
+++
@@ -1,9 +0,0 @@
-package org.duckdns.owly.bugs.openjpa_2778.service;
-
-import org.apache.deltaspike.data.api.EntityRepository;
-import org.apache.deltaspike.data.api.Repository;
-import org.duckdns.owly.bugs.openjpa_2778.entities.SomeEntity;
-
-@Repository
-public interface SomeRepository extends EntityRepository<SomeEntity, Long> {
-}
--- src/test/java/org/duckdns/owly/bugs/test/openjpa_2778/OpenJpa2778Test.java
+++ src/test/java/org/duckdns/owly/bugs/test/openjpa_2778/OpenJpa2778Test.java
@@ -1,40 +1,61 @@
 package org.duckdns.owly.bugs.test.openjpa_2778;
 
-import org.apache.deltaspike.testcontrol.api.junit.CdiTestRunner;
 import org.duckdns.owly.bugs.openjpa_2778.entities.SomeEntity;
-import org.duckdns.owly.bugs.openjpa_2778.service.SomeRepository;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
-@RunWith(CdiTestRunner.class)
 public class OpenJpa2778Test {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("SomeUnit");
 
-    @Inject
-    private SomeRepository someRepository;
+    private EntityManager entityManager;
+
+    @Before
+    public void createEntityManager() {
+        entityManager = emf.createEntityManager();
+    }
+
+    @After
+    public void closeEntityManager() {
+        if (entityManager.getTransaction().isActive())
+            entityManager.getTransaction().rollback();
+        entityManager.close();
+    }
 
     @Test
     public void testReproduceTheBugByInsert() {
         // Create it
+        entityManager.getTransaction().begin();
         SomeEntity someEntity = new SomeEntity();
         someEntity.setNonLobValue(getString(1001, 'a'));
         someEntity.setLobValue(getString(32000, 'a'));
-        someRepository.saveAndFlush(someEntity);
+        entityManager.persist(someEntity);
+        entityManager.flush();
+        entityManager.getTransaction().commit();
     }
 
     @Test
     public void testReproduceTheBugByUpdate() {
         // Create it
+        entityManager.getTransaction().begin();
         SomeEntity someEntity = new SomeEntity();
         someEntity.setNonLobValue("short");
         someEntity.setLobValue(getString(32000, 'a'));
-        someEntity = someRepository.saveAndFlushAndRefresh(someEntity);
+        entityManager.persist(someEntity);
+        entityManager.flush();
+        entityManager.getTransaction().commit();
 
         // Update it
+        entityManager.getTransaction().begin();
         someEntity.setNonLobValue(getString(1001, 'b'));
         someEntity.setLobValue(getString(32000, 'b'));
-        someRepository.saveAndFlush(someEntity);
+        entityManager.merge(someEntity);
+        entityManager.flush();
+        entityManager.getTransaction().commit();
     }
 
     private String getString(int length, char character) {
